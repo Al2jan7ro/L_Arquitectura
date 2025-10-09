0d61a4897de17004f98e71f1e5847e8c62b217d0
Al2jan7ro <xxzanderplaysxx27@gmail.com>
2025-10-09 11:57:51 -0400

    Refactored base URL references in layout, robots, sitemap, and SEO files to remove trailing slashes for consistency and improved SEO.

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://laarquitectura.vercel.app/';
+  const baseUrl = 'https://laarquitectura.vercel.app';
 
   return {
     rules: {
